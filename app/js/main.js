29cf97ffcf4ce6c3f485b4618cd49eaa6b19f3dc
MitPalVach <73882508+MitPalVach@users.noreply.github.com>
2020-12-15 22:16:05 +0300

    surf slider

--- app/js/main.js
+++ app/js/main.js
@@ -14,4 +14,14 @@ $(function () {
         asNavFor: '.header__slider',
     });
 
+
+    $('.surf-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="../images/arrows-left.svg" alt=""></img>',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="../images/arrows-right.svg" alt=""></img>',
+
+    });
+
+
 });
